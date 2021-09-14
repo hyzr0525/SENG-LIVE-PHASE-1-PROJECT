e4d2f0f997b1b25bc0cfa5892912eec89a4c7357
Devin Mullin <devin.mullin90@gmail.com>
2021-09-14 09:04:38 -0500

    updates to HTML, js code for button show/hide

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // type your main code here
+const BASE_URL = "http://localhost:3000/players"
+const cardsContainer = document.getElementById("cards-container")
+
+const button = document.querySelector('button') // player info button
+const cardsInfo = document.getElementById('cards-info') // info displayed by button
+let cardsInfoVisible = "hidden" // info default state of hidden
+
+function toggleInfo() { // function to toggle info as hidden or visible
+   cardsInfoVisible = cardsInfoVisible === "hidden" ? cardsInfoVisible = "visible" : cardsInfoVisible = "hidden";
+   cardsInfo.style.visibility = cardsInfoVisible
+    }
+
+button.onclick = toggleInfo
+
