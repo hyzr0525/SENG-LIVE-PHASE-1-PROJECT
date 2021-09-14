19b09886fdb0e30cb0e2811d65f042d7856b5159
Yewei <yeweichen1@gmail.com>
2021-09-13 21:52:25 -0500

    update

--- YeweiCode.js
+++ YeweiCode.js
@@ -1,1 +1,16 @@
-// type your code here
+/* HTML
+
+* Header 
+    -NBA logo
+    -background banner?
+* Player's Container 
+    -Cards with player's picture
+        1. Eventlisener - display player's detail info when clicked
+    -like button
+        1. Eventlisener - able to like player when clicked
+    -delete button
+        1. Eventlisener - able to delete player
+    -
+* new player form
+    -where user to submit new player
+        1. Eventlisener - able to add new player
