b70f2d073ef1e6e8cdb7454271923c4c2c6460c6
Yewei <yeweichen1@gmail.com>
2021-09-14 22:42:19 -0500

    HTML,CSS,and JS updates

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ function getCards(){
     fetch(BASE_URL)
     .then(res => res.json())
     .then(function (cardsArray){
-        cardsArray.forEach(function (cards){
+        cardsArray.forEach(function(cards){
             renderCards(cards)
         })
     })
 }
+
+const playerForm = document.querySelector("#form-container")
+
 
 function renderCards(cards){
     const playerCard = document.createElement('div')
@@ -23,24 +26,32 @@ function renderCards(cards){
     playerImg.src = cards.image
     playerImg.alt = cards.name
 
+    const likeBttn = document.createElement("button")
+    likeBttn.className = "like-bttn"
+    likeBttn.textContent = "♥"
+
+    const deleteBttn = document.createElement("button");
+    deleteBttn.className = "delete-bttn";
+    deleteBttn.textContent = "Delete";
+
     const playerInfo = document.createElement('div')
     playerInfo.id = "info"
 
-    const playerName = document.createElement('h2')
+    const playerName = document.createElement('h4')
     playerName.textContent = cards.name
 
     const playerNumber = document.createElement('h4')
-    playerNumber.textContent = `wears number ${cards.number} for the`
+    playerNumber.textContent = `Number: ${cards.number}`
     
-    const playerTeam = document.createElement('h3')
+    const playerTeam = document.createElement('h4')
     playerTeam.textContent = cards.team
 
-    const createButton = document.createElement('bttn')
+    const createButton = document.createElement('button')
+    createButton.className = "info-bttn"
     createButton.textContent = "Player Info"
 
-
-    playerCard.append(playerImg, createButton, playerInfo)
-    playerInfo.appendChild(playerName, playerNumber, playerTeam)
+    playerCard.append(playerImg, likeBttn, createButton, playerInfo, deleteBttn)
+    playerInfo.append(playerName, playerNumber, playerTeam)
     cardsContainer.appendChild(playerCard)
 
     const button = document.querySelector('button') // player info button
@@ -52,12 +63,23 @@ function renderCards(cards){
         cardsInfo.style.visibility = cardsInfoVisible
     }
 
-button.onclick = toggleInfo
+    button.onclick = toggleInfo
 
-    }
+}
 
-
-
-
-
-
+function createNewCard(event) {
+    event.preventDefault();
+    const newPlayer = document.querySelector("#player-input").value;
+    const newTeam = document.querySelector("#team-input").value;
+    const newNumber = document.querySelector("#number-input").value;
+    const newImg = document.querySelector("#image-input").value;
+    
+    const newPlayerCard = {
+      name: newPlayer,
+      number: newNumber,
+      team: newTeam,
+      img: newImg
+    };
+    renderCards(newPlayerCard);
+    playerForm.reset();
+}
